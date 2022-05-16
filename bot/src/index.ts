2dd52a9ca873a8c5a4fd304123ed5c1a48647216
auoie <105099407+auoie@users.noreply.github.com>
2022-05-15 23:49:37 -0700

    including time in result

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -31,9 +31,22 @@ import {
 // );
 
 // 4.
-getCoursesPage().then((res) => {
-  const courses = parseCoursesPage(res);
+export const getBotResult = async () => {
+  const timeString =  new Date().toJSON()
+  const coursesPage = await getCoursesPage()
+  const courses = parseCoursesPage(coursesPage)
   if (courses) {
-    console.log(JSON.stringify(cleanCourses(courses)));
+    const result = {
+      time: timeString,
+      courses: cleanCourses(courses)
+    }
+    return result
   }
-});
+  throw new Error("parsing unsuccessful")
+}
+export type BotResult = Awaited<ReturnType<typeof getBotResult>>
+getBotResult().then(result => {
+  if (result !== undefined) {
+    console.log(JSON.stringify(result))
+  }
+})
