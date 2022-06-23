7ec9f5197bcdf1e2866ec1ded629c9e17299bb8b
auoie <105099407+auoie@users.noreply.github.com>
2022-06-23 06:03:45 -0700

    fixed typo

--- apps/frontend-tailwind/pages/_app.tsx
+++ apps/frontend-tailwind/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider disableTransitionOnChange attribute="class">
       <Head>
-        <title>eggheaad.io Courses</title>
+        <title>egghead.io Courses</title>
         <link rel="icon" href={favicon} type="image/x-icon" />
       </Head>
       <Navbar />
